44ed618dd135e561aa31c8032202a0cc4b6f933e
Spy <naphat.a@rmutsvmail.com>
2025-01-10 09:33:07 +0700

    เพิ่มข้อความ Placeholder

--- index.vue
+++ index.vue
@@ -2,7 +2,7 @@
   <div>
     <h1>LAB3 - 058ForFork</h1>
     <hr>
-    <input type="text" name="userName" id="test" v-model="userName">
+    <input type="text" name="userName" id="test" v-model="userName" placeholder="ป้อน Username ของคุณ">
     <p>Your Username: {{ userName }}</p>
   </div>
 </template>
