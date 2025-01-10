19427275508aff11ba2e535e16a6db236cdf63e9
bKoZii <bigkonkamon@gmail.com>
2025-01-10 08:41:18 +0700

    feat: :sparkles: เพิ่มไฟล์ Code
    เพิ่มไฟล์ contact.php / index.vue

---
+++ index.vue
@@ -0,0 +1,12 @@
+<template>
+  <div>
+    <h1>LAB3 - 058ForFork</h1>
+    <hr>
+    <input type="text" name="userName" id="test" v-model="userName">
+  </div>
+</template>
+
+<script setup>
+import { ref } from 'vue'
+const userName = ref('')
+</script>
