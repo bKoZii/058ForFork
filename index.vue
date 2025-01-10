73ba701839d416bc825be8db58446ec435305b57
bKoZii <bigkonkamon@gmail.com>
2025-01-10 08:52:10 +0700

    feat(vue): :sparkles: ใช้ TypeScript & เพิ่มข้อความแสดง Input

--- index.vue
+++ index.vue
@@ -3,10 +3,11 @@
     <h1>LAB3 - 058ForFork</h1>
     <hr>
     <input type="text" name="userName" id="test" v-model="userName">
+    <p>Your Username: {{ userName }}</p>
   </div>
 </template>
 
-<script setup>
+<script setup lang="ts">
 import { ref } from 'vue'
-const userName = ref('')
+const userName = ref<String>('')
 </script>
